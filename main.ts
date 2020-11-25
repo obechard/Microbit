0b3999bc4221dbf3c4f7c681df5a1e624fa4ca67
ExploratekCat <74997902+ExploratekCat@users.noreply.github.com>
2020-11-24 20:55:36 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    Potato = receivedNumber
+})
+input.onButtonPressed(Button.A, function () {
+    Potato = randint(10, 20)
+})
+input.onGesture(Gesture.Shake, function () {
+    if (Potato) {
+        radio.sendNumber(Potato)
+        Potato = -1
+    }
+})
+let Potato = 0
+Potato = -1
+radio.setGroup(1)
 basic.forever(function () {
-	
+    if (Potato) {
+        basic.showIcon(IconNames.Skull)
+    }
+    if (Potato) {
+        basic.clearScreen()
+        if (true) {
+            basic.showIcon(IconNames.Target)
+            Potato += -1
+        }
+    }
 })
